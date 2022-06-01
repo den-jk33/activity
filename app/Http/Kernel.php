01720de020fe607bcafa938d11400b6353ffbad9
Den <39164594+den-jk33@users.noreply.github.com>
2022-06-01 14:08:14 +0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'activity_log' => \App\Http\Middleware\ActivityLog::class,
         'admin' => \App\Http\Middleware\Admin::class,
     ];
 }
